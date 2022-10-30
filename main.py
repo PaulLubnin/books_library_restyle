7adf6346a722f763e1cc6044f828feaf22a4c7e8
paulego <egopaul@inbox.ru>
2022-10-30 10:52:01 +0500

    Added url serializer.

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ TULULU_URl = 'https://tululu.org'
 
 
 def check_url(url: str):
-    """Проверка урла."""
+    """Проверка урла, что сайт верный."""
 
-    unq_url = unquote(url)
-    if urlsplit(unq_url).netloc == urlsplit(TULULU_URl).netloc:
+    if url_serializing(url)['site_name'] == url_serializing(TULULU_URl)['site_name']:
         return
     raise requests.HTTPError
+
+
+def url_serializing(url: str) -> dict:
+    """Функция парсит урл."""
+
+    unq_url = unquote(url)
+    split_url = urlsplit(unq_url)
+    serialised_data = {'site_name': split_url.netloc,
+                       'site_path': split_url.path}
+    if split_url.query:
+        query_key, book_id = split_url.query.split('=')
+        serialised_data[query_key] = book_id
+    return serialised_data
 
 
 def create_path(folder_name: str, book_name: str) -> Path:
@@ -36,7 +48,7 @@ def save_book(json, book_id: int):
 def check_for_redirect(response):
     """Функция проверки редиректа."""
 
-    if not response.history and response.url != TULULU_URl:
+    if not response.history:
         return
     else:
         raise requests.HTTPError
@@ -69,21 +81,23 @@ def fetch_book_data(book_id: int):
     return header, author
 
 
+def download_txt(url: str, filename: str, folder='books/') -> str:
+    # book = get_books_file(url)
+    print(url_serializing(url))
+    return
+
+
 def main(url: str):
     try:
         check_url(url)
-
-        for book_id in range(1, 11):
-            book = get_books_file(book_id)
-            if book:
-                save_book(book, book_id)
     except requests.HTTPError:
         print('Wrong url')
 
 
 if __name__ == '__main__':
     url_file = 'http://tululu.org/txt.php?id=1'
-    main(url_file)
+    # url_file = f'{TULULU_URl}/b1'
+    # main(url_file)
 
-    # print(type(serialized_url(url)), serialized_url(url))
-    # print(type(serialized_url(url)), serialized_url(url)[1])
+    filepath = download_txt(url_file, 'Алиби')
+    print('filepath', filepath)
